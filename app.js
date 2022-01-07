a839b2e377d179c5995254bc9cb012ef8bfdcdfe
Kob <57989060+the-Kob@users.noreply.github.com>
2022-01-07 00:18:24 +0000

    Update app.js

--- app.js
+++ app.js
@@ -145,9 +145,9 @@ function setup(shaders)
                 if(lights[i].rotationAxis == -1) {
                     lights[i].rotationAxis = randomInteger(0, 2);
                 } else {
-                    window.alert("The lights are already rotating!" + \n
-                                 + "The lights rotate on a random axis (X, Y or Z), so it is possible the lights are rotating and you just don't notice it." + \n
-                                 + "Try again!");
+                    window.alert("The lights are already rotating!\n\n" +
+                                 "The lights rotate on a random axis (X, Y or Z), so it is possible the lights are rotating and you just don't notice it.\n" + 
+                                 "Try again!");
                     break;
                 }
             }
